d055776e84e8cbd7405da07bb986c25e1ceafb35
christian-mapundu <Christianmapundu@gmail.com>
2025-03-23 01:45:16 +0200

    Added some functions

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Welcome to the Program");
+    }
+
+    static void PrompUserName()
+    {
+        Console.Write("Enter Your Username: ");
+        string userName = Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Enter Your Favorite Number: ");
+        int favoriteNumber = int.Parse(Console.ReadLine());
+
+        return favoriteNumber;
+    }
     }
 }
